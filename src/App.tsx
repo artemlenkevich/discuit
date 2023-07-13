5e547de40f8bdde79e3110ca5d0f6520ef90ca72
Artem Lenkevich <ar.lenkevich@gmail.com>
2023-07-13 12:57:32 +0300

    started FeedLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import { useState } from 'react';
 
-import { registerUserThunk, selectAuth } from './features/auth/store';
-import { useAppDispatch, useAppSelector } from './hooks/store';
-import { Layout } from './components';
+import { MainLayout } from '@/components/layouts';
 
 function App() {
-  const dispatch = useAppDispatch();
-  const auth = useAppSelector(selectAuth);
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-
-  console.log(auth);
-
-  const onSubmit = () => {
-    dispatch(registerUserThunk({ email, password, username }));
-  };
-
   return (
-    <Layout>
-      <div>
-        <input type='text' placeholder='email' onChange={(e) => setEmail(e.target.value)} />
-        <input type='text' placeholder='password' onChange={(e) => setPassword(e.target.value)} />
-        <input type='text' placeholder='username' onChange={(e) => setUsername(e.target.value)} />
-        <button onClick={onSubmit}>Submit</button>
-      </div>
-    </Layout>
+    <MainLayout>
+      <div>x</div>
+    </MainLayout>
   );
 }
 
