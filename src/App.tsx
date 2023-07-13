dd082fa8ce2be730b58f22934a0b242cb64b5b5c
Artem Lenkevich <ar.lenkevich@gmail.com>
2023-07-13 13:53:17 +0300

    app setup moved to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Provider } from 'react-redux';
+
 import { AppRoutes } from '@/routes';
 
+import { AuthProvider } from './providers/AuthProvider';
+import { store } from './store';
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </Provider>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-
-import { store } from '@/store';
 
 import App from './App';
-import { AuthProvider } from './providers/AuthProvider';
 import reportWebVitals from './reportWebVitals';
 import '@/scss/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <Provider store={store}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </Provider>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
