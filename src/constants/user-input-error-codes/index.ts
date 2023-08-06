0fccd39b63464e0f432713a3a294fad96542f303
Artem Lenkevich <ar.lenkevich@gmail.com>
2023-08-06 12:26:05 +0300

    added user-not-found error

--- src/constants/user-input-error-codes/index.ts
+++ src/constants/user-input-error-codes/index.ts
@@ -1,4 +1,6 @@
 export const userInputErrorCodes: Record<string, string> = {
+  'auth/user-not-found':
+    'There is no user record corresponding to this identifier. The user may have been deleted.',
   'auth/wrong-password': 'The password is invalid or the user does not have a password.',
   'auth/invalid-email': 'The email address is badly formatted.',
   'auth/email-already-in-use': 'The email address is already in use by another account.',
