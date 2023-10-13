a22d033d7eba11f614270f1d2cb3cbb3834933eb
Artem Lenkevich <a.lenkevich@softteco.com>
2023-10-13 14:09:59 +0300

    moved Header to a distinct component

---
+++ src/layouts/MainLayout/Header/Header.tsx
@@ -0,0 +1,47 @@
+import { Burger } from '@/components/ui/Burger';
+import { Input } from '@/components/ui/Input';
+
+import { ProfileWidget } from '../ProfileWidget';
+import { UnauthorizedWidget } from '../UnauthorizedWidget';
+
+import { ReactComponent as SearchLogo } from './assets/logo.svg';
+import styles from './Header.module.scss';
+
+interface HeaderProps {
+  onBurgerClick: () => void;
+  isBurgerShown: boolean;
+  isSearchShown: boolean;
+  isAuthenticated: boolean;
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  onBurgerClick,
+  isBurgerShown,
+  isSearchShown,
+  isAuthenticated,
+}) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <div className={styles.left}>
+          {isBurgerShown && (
+            <div className={styles.burgerWrapper}>
+              <Burger onClick={onBurgerClick} />
+            </div>
+          )}
+          <a href='/' className={styles.logo}>
+            Discuit
+          </a>
+          {isSearchShown && (
+            <div className={styles.search}>
+              <Input fullWidth startIcon={<SearchLogo />} type='text' placeholder='Search' />
+            </div>
+          )}
+        </div>
+        <div className={styles.right}>
+          {isAuthenticated ? <ProfileWidget /> : <UnauthorizedWidget />}
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,25 +1,22 @@
-import { useEffect, useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
 import { MobileNavigation } from '@/components/MobileNavigation';
-import { Burger } from '@/components/ui/Burger';
-import { Input } from '@/components/ui/Input';
 import { Breakpoints } from '@/constants/breakpoints';
 import { useAppSelector } from '@/hooks/store';
 import useScreenSize from '@/hooks/useScreenSize';
 import { selectIsAuthenticated } from '@/store/userSlice';
 
-import { ReactComponent as SearchLogo } from './assets/logo.svg';
+import { Header } from './Header/Header';
 import styles from './MainLayout.module.scss';
-import { ProfileWidget } from './ProfileWidget';
-import { UnauthorizedWidget } from './UnauthorizedWidget';
 
 export const MainLayout: React.FC = () => {
   const { width } = useScreenSize();
   const [showMobileNavigation, setShowMobileNavigation] = useState(false);
   const isAuthenticated = useAppSelector(selectIsAuthenticated);
-  const showBurger = width < Breakpoints.xl;
-  const showSearch = width > Breakpoints.md;
+
+  const isBurgerShown = width < Breakpoints.xl;
+  const isSearchShown = width > Breakpoints.md;
 
   const onMobileNavigationClose = () => {
     setShowMobileNavigation(false);
@@ -31,32 +28,18 @@ export const MainLayout: React.FC = () => {
 
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.inner}>
-          <div className={styles.left}>
-            {showBurger && (
-              <div className={styles.burgerWrapper}>
-                <Burger onClick={onBurgerClick} />
-              </div>
-            )}
-            <a href='/' className={styles.logo}>
-              Discuit
-            </a>
-            {showSearch && (
-              <div className={styles.search}>
-                <Input fullWidth startIcon={<SearchLogo />} type='text' placeholder='Search' />
-              </div>
-            )}
-          </div>
-          <div className={styles.right}>
-            {isAuthenticated ? <ProfileWidget /> : <UnauthorizedWidget />}
-          </div>
-        </div>
-      </header>
+      <Header
+        onBurgerClick={onBurgerClick}
+        isBurgerShown={isBurgerShown}
+        isSearchShown={isSearchShown}
+        isAuthenticated={isAuthenticated}
+      />
       <div className={styles.content}>
         <Outlet />
       </div>
-      {showMobileNavigation && showBurger && <MobileNavigation onClose={onMobileNavigationClose} />}
+      {showMobileNavigation && isBurgerShown && (
+        <MobileNavigation onClose={onMobileNavigationClose} />
+      )}
     </>
   );
 };
