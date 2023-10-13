ebac7d908ab5631611d91f6aede78380ad519df6
Artem Lenkevich <a.lenkevich@softteco.com>
2023-10-13 13:38:19 +0300

    added responsivness to a PostLayout

--- src/layouts/PostLayout/index.tsx
+++ src/layouts/PostLayout/index.tsx
@@ -1,9 +1,12 @@
+import cn from 'classnames';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { CreatePostWidget } from '@/components/CreatePostWidget';
 import { Post } from '@/components/Post';
+import { Breakpoints } from '@/constants/breakpoints';
 import { useAppDispatch, useAppSelector } from '@/hooks/store';
+import useScreenSize from '@/hooks/useScreenSize';
 import { getPostThunk, selectPost } from '@/store/postsSlice';
 
 import styles from './PostLayout.module.scss';
@@ -12,6 +15,8 @@ export const PostLayout: React.FC = () => {
   const { postId } = useParams();
   const dispatch = useAppDispatch();
   const post = useAppSelector(selectPost);
+  const { width } = useScreenSize();
+  const showRightBar = width > Breakpoints.lg;
 
   useEffect(() => {
     if (postId) {
@@ -22,14 +27,16 @@ export const PostLayout: React.FC = () => {
   return (
     <div className={styles.layout}>
       <div className={styles.container}>
-        <div>
+        <main className={cn(styles.mainContent, { [styles.disabledRigthBar]: !showRightBar })}>
           {post && (
             <Post title={post.title} name={post.name} createdAt={post.createdAt} id={post.id} />
           )}
-        </div>
-        <div className={styles.rightSidebar}>
-          <CreatePostWidget />
-        </div>
+        </main>
+        {showRightBar && (
+          <div className={styles.rightSidebar}>
+            <CreatePostWidget />
+          </div>
+        )}
       </div>
     </div>
   );
